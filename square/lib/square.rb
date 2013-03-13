8b996a349cf9e9de3e21a968c461e86ff4c47361
dklounge <daviddarden11@gmail.com>
2013-03-12 17:24:29 -0700

    updated square test

--- square/lib/square.rb
+++ square/lib/square.rb
@@ -1,3 +1,6 @@
 class Square
   # methods go here
+  def initialize(leng_of_sides)
+    @leng_of_sides = 4
+  end
 end
--- square/spec/square_spec.rb
+++ square/spec/square_spec.rb
@@ -3,5 +3,8 @@ require 'square'
 
 describe Square do 
   # put test here
-  
+  it 'initalizes with the length of its sides as an argument' do
+    square = Square.new(5)
+    square.should be_an_instance_of Square
+  end
 end
